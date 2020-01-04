367f67c6221d4d02584bcfc004f38479d200ba12
waxs <sander@vruchtvlees.com>
2020-01-04 22:52:23 +0100

    🤡 examples and mockup

---
+++ example/demo/data.js
@@ -0,0 +1,76 @@
+/** ----------------------------------------
+     Mockup Data
+ ---------------------------------------- */
+
+const data = [
+    {
+        active: true,
+        created: '12/2/2020',
+        name: 'Sander',
+        age: 30,
+        city: 'Amsterdam'
+    },
+    {
+        active: false,
+        created: '10/1/2017',
+        name: 'Nicole',
+        age: 32,
+        city: 'Paris'
+    },
+    {
+        active: true,
+        created: '7/10/2019',
+        name: 'John',
+        age: 45,
+        city: 'New York'
+    },
+    {
+        active: false,
+        created: '3/20/2019',
+        name: 'Sara',
+        age: 26,
+        city: 'Rome'
+    },
+    {
+        active: false,
+        created: '3/20/2020',
+        name: 'Peter',
+        age: 43,
+        city: 'London'
+    },
+    {
+        active: true,
+        created: '10/12/2018',
+        name: 'Lisa',
+        age: 55,
+        city: 'Glasgow'
+    },
+    {
+        active: true,
+        created: '7/23/2019',
+        name: 'Stephen',
+        age: 22,
+        city: 'Berlin'
+    },
+    {
+        active: true,
+        created: '5/1/2018',
+        name: 'Lauren',
+        age: 18,
+        city: 'Moscow'
+    },
+    {
+        active: true,
+        created: '7/23/2019',
+        name: 'Phillip',
+        age: 27,
+        city: 'London'
+    },
+    {
+        active: true,
+        created: '7/23/2019',
+        name: 'Trevor',
+        age: 38,
+        city: 'San Francisco'
+    }
+];
