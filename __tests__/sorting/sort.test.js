10d1f5089333afccad2902d2178cf508027a6599
waxs <sander@vruchtvlees.com>
2020-01-07 20:55:17 +0100

    ✅ create sorting test

---
+++ __tests__/sorting/sort.test.js
@@ -0,0 +1,44 @@
+/** ----------------------------------------
+     Mockup Data
+ ---------------------------------------- */
+
+import { data } from '@mock/advanced';
+
+/** ----------------------------------------
+     Needle
+ ---------------------------------------- */
+
+import Needle from '@js/Needle';
+const needle = new Needle(data);
+
+/** ----------------------------------------
+     Test sort()
+ ---------------------------------------- */
+
+describe('Using sort() method for sorting items', () => {
+    test('It should display sorted array', () => {
+        const input = data;
+
+        const output = [
+            {
+                name: 'Sander',
+                age: 30
+            },
+            {
+                name: 'Nicole',
+                age: 32
+            },
+            {
+                name: 'John',
+                age: 45
+            }
+        ];
+
+        const query = needle
+            .sort('age')
+            .select('name', 'age')
+            .take();
+
+        expect(query).toEqual(output);
+    });
+});
