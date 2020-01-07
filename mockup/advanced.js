d76a0801a58a7ffea044f3433773e84de1d4196b
waxs <sander@vruchtvlees.com>
2020-01-07 20:33:51 +0100

    🚚 moving mockup data

---
+++ mockup/advanced.js
@@ -0,0 +1,53 @@
+/** ----------------------------------------
+    Mockup Data
+ ---------------------------------------- */
+
+export const data = [
+    {
+        active: true,
+        created: '12/2/2020',
+        name: 'Sander',
+        age: 30,
+        city: 'Amsterdam',
+        score: 88,
+        categories: [
+            'development',
+            'ux'
+        ],
+        contact: {
+            mail: 'sander@example.com',
+            phone: {
+                mobile: '06-12345678'
+            }
+        }
+    },
+    {
+        active: false,
+        created: '10/1/2017',
+        name: 'Nicole',
+        age: 32,
+        city: 'Paris',
+        score: 66,
+        categories: [
+            'design',
+            'ux'
+        ],
+        contact: {
+            mail: 'nicole@example.com'
+        }
+    },
+    {
+        active: true,
+        created: '7/10/2019',
+        name: 'John',
+        age: 45,
+        city: 'New York',
+        score: 78,
+        categories: [
+            'ux'
+        ],
+        contact: {
+            mail: 'john@example.com'
+        }
+    }
+];
