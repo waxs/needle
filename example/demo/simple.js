28cecd65b1c44fca660be3bc3958c9c7189a8abf
waxs <sander@vruchtvlees.com>
2020-01-07 20:09:24 +0100

    🤡 create some mockup data for testing

---
+++ example/demo/simple.js
@@ -0,0 +1,27 @@
+/** ----------------------------------------
+    Mockup Data
+ ---------------------------------------- */
+
+export const data = [
+    {
+        active: true,
+        created: '12/2/2020',
+        name: 'Sander',
+        age: 30,
+        city: 'Amsterdam'
+    },
+    {
+        active: false,
+        created: '10/1/2017',
+        name: 'Nicole',
+        age: 32,
+        city: 'Paris'
+    },
+    {
+        active: true,
+        created: '7/10/2019',
+        name: 'John',
+        age: 45,
+        city: 'New York'
+    }
+];
