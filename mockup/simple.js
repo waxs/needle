e477188b17b7d22b1bdde675a59c6c4ab9f4be73
waxs <sander@vruchtvlees.com>
2020-01-09 22:19:02 +0100

    🤡 extend mockup data

--- mockup/simple.js
+++ mockup/simple.js
@@ -8,69 +8,110 @@ const data = [
         created: '12/2/2020',
         name: 'Sander',
         age: 30,
-        city: 'Amsterdam'
+        city: 'Amsterdam',
+        hobbies: [
+            'Music',
+            'Movies'
+        ]
     },
     {
         active: false,
         created: '10/1/2017',
         name: 'Nicole',
         age: 32,
-        city: 'Paris'
+        city: 'Paris',
+        hobbies: [
+            'Dancing'
+        ]
     },
     {
         active: true,
         created: '7/10/2019',
         name: 'John',
         age: 45,
-        city: 'New York'
+        city: 'New York',
+        hobbies: [
+            'Sports',
+            'Books'
+        ]
     },
     {
         active: false,
         created: '3/20/2019',
         name: 'Sara',
         age: 26,
-        city: 'Rome'
+        city: 'Rome',
+        hobbies: [
+            'Sports',
+            'Music'
+        ]
     },
     {
         active: false,
         created: '3/20/2020',
         name: 'Peter',
         age: 43,
-        city: 'London'
+        city: 'London',
+        hobbies: [
+            'Music',
+            'Dancing',
+            'Games'
+        ]
     },
     {
         active: true,
         created: '10/12/2018',
         name: 'Lisa',
         age: 55,
-        city: 'Glasgow'
+        city: 'Glasgow',
+        hobbies: [
+            'Books'
+        ]
     },
     {
         active: true,
         created: '7/23/2019',
         name: 'Stephen',
         age: 22,
-        city: 'Berlin'
+        city: 'Berlin',
+        hobbies: [
+            'Books',
+            'Sports',
+            'Music'
+        ]
     },
     {
         active: true,
         created: '5/1/2018',
         name: 'Lauren',
         age: 18,
-        city: 'Moscow'
+        city: 'Moscow',
+        hobbies: [
+            'Series',
+            'Movies'
+        ]
     },
     {
         active: true,
         created: '7/23/2019',
         name: 'Phillip',
         age: 27,
-        city: 'London'
+        city: 'London',
+        hobbies: [
+            'Books',
+            'Walking'
+        ]
     },
     {
         active: true,
         created: '7/23/2019',
         name: 'Trevor',
         age: 38,
-        city: 'San Francisco'
+        city: 'San Francisco',
+        hobbies: [
+            'Movies',
+            'Music',
+            'Games'
+        ]
     }
 ];
