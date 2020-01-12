6926deee3d9b38740890ac4db5fe12ce32539d3e
waxs <sander@vruchtvlees.com>
2020-01-12 22:25:06 +0100

    ⚙ config alias

--- webpack/webpack.common.config.js
+++ webpack/webpack.common.config.js
@@ -6,19 +6,25 @@ const fs = require('fs');
 const path = require('path');
 
 /** ----------------------------------------
+     Settings
+ ---------------------------------------- */
+
+const BASE_DIR = __dirname + '/./../';
+
+/** ----------------------------------------
     Configuration
  ---------------------------------------- */
 
 module.exports = {
     devServer: {
-        contentBase: path.join(__dirname, './../'),
+        contentBase: path.join(BASE_DIR),
         port: 3000,
         watchContentBase: true
     },
     entry: './src/needle.js',
     output: {
         filename: 'needle.js',
-        path: path.resolve(__dirname, './../dist'),
+        path: path.resolve(BASE_DIR + 'dist'),
         umdNamedDefine: true,
         libraryExport: 'default',
         libraryTarget: 'var',
@@ -38,10 +44,10 @@ module.exports = {
     },
     resolve: {
         alias: {
-            '@build': path.resolve(__dirname, 'dist/'),
-            '@js': path.resolve(__dirname, 'src/'),
-            '@mock': path.resolve(__dirname, 'mockup/'),
-            '@util': path.resolve(__dirname, 'src/util/')
+            '@build': path.resolve(BASE_DIR, 'dist/'),
+            '@js': path.resolve(BASE_DIR, 'src/'),
+            '@mock': path.resolve(BASE_DIR, 'mockup/'),
+            '@util': path.resolve(BASE_DIR, 'src/util/')
         },
         extensions: ['*', '.js']
     }
