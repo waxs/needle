553bc4db2f7a2126f47993551e62926c13c54db8
waxs <sander@vruchtvlees.com>
2020-01-14 21:06:09 +0100

    ⚙ add month + period config and tweak default config

---
+++ src/config/months.config.js
@@ -0,0 +1,78 @@
+/** ----------------------------------------
+     Utilities
+ ---------------------------------------- */
+
+import { leapYear } from '@util/_leapYear';
+
+/** ----------------------------------------
+    Configuration
+ ---------------------------------------- */
+
+const months = [
+    {
+        index: 0,
+        month: 'jan',
+        days: 31
+    },
+    {
+        index: 1,
+        month: 'feb',
+        days: leapYear ? 28 : 29
+    },
+    {
+        index: 2,
+        month: 'mar',
+        days: 31
+    },
+    {
+        index: 3,
+        month: 'apr',
+        days: 30
+    },
+    {
+        index: 4,
+        month: 'may',
+        days: 31
+    },
+    {
+        index: 5,
+        month: 'jun',
+        days: 30
+    },
+    {
+        index: 6,
+        month: 'jul',
+        days: 31
+    },
+    {
+        index: 7,
+        month: 'aug',
+        days: 31
+    },
+    {
+        index: 8,
+        month: 'sep',
+        days: 30
+    },
+    {
+        index: 9,
+        month: 'oct',
+        days: 31
+    },
+    {
+        index: 10,
+        month: 'nov',
+        days: 30
+    },
+    {
+        index: 11,
+        month: 'dec',
+        days: 31
+    }
+];
+
+/** ----------------------------------------
+    Exports
+ ---------------------------------------- */
+
+export default months;
---
+++ src/config/period.config.js
@@ -0,0 +1,48 @@
+/** ----------------------------------------
+    Utilities
+ ---------------------------------------- */
+
+import { leapYear } from '@util/_leapYear';
+
+/** ----------------------------------------
+    Configuration
+ ---------------------------------------- */
+
+const period = () => {
+    const date = new Date();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+
+    return [
+        {
+            name: 'days',
+            amount: 1
+        },
+        {
+            name: 'weeks',
+            amount: 7
+        },
+        {
+            name: 'months',
+            year: year,
+            month: month,
+            amount: 30
+        },
+        {
+            name: 'quarters',
+            year: year,
+            amount: Math.round((leapYear ? 366 : 365) / 4)
+        },
+        {
+            name: 'years',
+            year: year,
+            amount: leapYear ? 366 : 365
+        }
+    ];
+];
+
+/** ----------------------------------------
+    Exports
+ ---------------------------------------- */
+
+export default period;
