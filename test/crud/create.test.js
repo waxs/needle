d49873ae4684b7ca21ad495c2457547024652173
waxs <sander@vruchtvlees.com>
2020-01-15 08:32:27 +0100

    ✅ add create() test

---
+++ test/crud/create.test.js
@@ -0,0 +1,34 @@
+/** ----------------------------------------
+    Mockup Data
+ ---------------------------------------- */
+
+import { data } from '@mock/advanced';
+
+/** ----------------------------------------
+    Needle
+ ---------------------------------------- */
+
+import Needle from '@build/needle.min';
+const needle = new Needle(data);
+
+/** ----------------------------------------
+    Test create()
+ ---------------------------------------- */
+
+describe('Using create() method to add new item', () => {
+    test('It should display all names', () => {
+        const input = data;
+
+        const output = ['Sander', 'Nicole', 'John', 'Peter'];
+
+        needle.create({
+            name: 'Peter',
+            age: 44
+        });
+
+        const query = needle
+            .values('name')
+
+        expect(query).toEqual(output);
+    });
+});
