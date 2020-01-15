a4780f49c467a9b47f9d48fdc3f4f51b52da1a55
waxs <sander@vruchtvlees.com>
2020-01-15 08:00:52 +0100

    ✅ add year() test

---
+++ test/dates/year.test.js
@@ -0,0 +1,37 @@
+/** ----------------------------------------
+    Mockup Data
+ ---------------------------------------- */
+
+import { data } from '@mock/advanced';
+
+/** ----------------------------------------
+     Needle
+ ---------------------------------------- */
+
+import Needle from '@build/needle.min';
+const needle = new Needle(data);
+
+/** ----------------------------------------
+     Test month()
+ ---------------------------------------- */
+
+describe('Using month() method for date match', () => {
+    test('It should display item: Sander', () => {
+        const input = data;
+
+        const output = [
+            {
+                name: 'Sander',
+                age: 30,
+                city: 'Amsterdam'
+            }
+        ];
+
+        const query = needle
+            .year('created', 2020)
+            .select('name', 'age', 'city')
+            .take();
+
+        expect(query).toEqual(output);
+    });
+});
