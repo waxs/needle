a9c172abeb8d547c52e1c9eae60d8fd269e6c9e1
waxs <sander@vruchtvlees.com>
2020-01-15 08:32:45 +0100

    ✅ add read() test

---
+++ test/crud/read.test.js
@@ -0,0 +1,34 @@
+/** ----------------------------------------
+    Mockup Data
+ ---------------------------------------- */
+
+import { data } from '@mock/advanced';
+
+/** ----------------------------------------
+    Needle
+ ---------------------------------------- */
+
+import Needle from '@build/needle.min';
+const needle = new Needle(data);
+
+/** ----------------------------------------
+    Test read()
+ ---------------------------------------- */
+
+describe('Using read() to retrieve a single item', () => {
+    test('It should display item: Sander', () => {
+        const input = data;
+
+        const output = [{
+            name: 'Sander',
+            age: 30
+        }];
+
+        const query = needle
+            .read(0)
+            .select('name', 'age')
+            .take();
+
+        expect(query).toEqual(output);
+    });
+});
