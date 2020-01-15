68498ea3251f73f959b8fb288ca2eb54fd7b3d8e
waxs <sander@vruchtvlees.com>
2020-01-15 07:45:53 +0100

    ✅ add month() test

---
+++ test/dates/month.test.js
@@ -0,0 +1,36 @@
+/** ----------------------------------------
+    Mockup Data
+ ---------------------------------------- */
+
+import { data } from '@mock/advanced';
+
+/** ----------------------------------------
+     Needle
+ ---------------------------------------- */
+
+import Needle from '@build/needle.min';
+const needle = new Needle(data);
+
+/** ----------------------------------------
+     Test month()
+ ---------------------------------------- */
+
+describe('Using month() method for date match', () => {
+    test('It should display item: Sander', () => {
+        const input = data;
+
+        const output = [
+            {
+                name: 'Sander',
+                age: 30
+            }
+        ];
+
+        const query = needle
+            .month('created', 'feb', 2020)
+            .select('name', 'age')
+            .take();
+
+        expect(query).toEqual(output);
+    });
+});
