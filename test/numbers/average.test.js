e1353592b35fbc5a6029a29ace4029978daaaee7
waxs <sander@vruchtvlees.com>
2020-01-26 20:59:18 +0100

    ✅ add average() test

---
+++ test/numbers/average.test.js
@@ -0,0 +1,37 @@
+/** ----------------------------------------
+    Mockup Data
+ ---------------------------------------- */
+
+import { data } from '@mock/advanced';
+
+/** ----------------------------------------
+    Needle
+ ---------------------------------------- */
+
+import Needle from '@build/needle.min';
+const needle = new Needle(data);
+
+/** ----------------------------------------
+    Test average()
+ ---------------------------------------- */
+
+describe('Using max() method for highest value match', () => {
+    test('It should display item: Nicole', () => {
+        const input = data;
+
+        const output = [
+            {
+                name: 'Nicole',
+                age: 32,
+                city: 'Paris'
+            }
+        ];
+
+        const query = needle
+            .average('age')
+            .select('name', 'age', 'city')
+            .take();
+
+        expect(query).toEqual(output);
+    });
+});
