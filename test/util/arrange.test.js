9a74f234bb02b057053a27b97b398127adc93394
waxs <sander@vruchtvlees.com>
2020-02-09 13:17:33 +0100

    ✅ create arrange() util test

---
+++ test/util/arrange.test.js
@@ -0,0 +1,77 @@
+/** ----------------------------------------
+     Needle
+ ---------------------------------------- */
+
+import arrange from '@util/_arrange';
+
+/** ----------------------------------------
+     Test asc arrange()
+ ---------------------------------------- */
+
+describe('Using array() utility for sorting items', () => {
+    test('It should sort the array on numbers asc', () => {
+        const input = [
+            {
+                index: 1
+            },
+            {
+                index: 3
+            },
+            {
+                index: 2
+            }
+        ];
+
+        const output = [
+            {
+                index: 1
+            },
+            {
+                index: 2
+            },
+            {
+                index: 3
+            }
+        ];
+
+        const util = arrange(input, 'index', 'asc');
+
+        expect(util).toEqual(output);
+    });
+});
+
+/** ----------------------------------------
+    Test desc arrange()
+ ---------------------------------------- */
+
+describe('Using array() utility for sorting items', () => {
+    test('It should sort the array on numbers desc', () => {
+        const input = [
+            {
+                index: 1
+            },
+            {
+                index: 3
+            },
+            {
+                index: 2
+            }
+        ];
+
+        const output = [
+            {
+                index: 3
+            },
+            {
+                index: 2
+            },
+            {
+                index: 1
+            }
+        ];
+
+        const util = arrange(input, 'index', 'desc');
+
+        expect(util).toEqual(output);
+    });
+});
