040a08f3a9714f0b95285b56e7f50d36b9f45d34
waxs <sander@vruchtvlees.com>
2020-02-18 18:08:50 +0100

    🔥 remove period() ♻️ refactor to between()

--- src/methods/dates/base.js
+++ src/methods/dates/base.js
@@ -4,7 +4,6 @@
 
 import month from './_month';
 import _namedDates from './_namedDates';
-import period from './_period';
 import previous from './_previous';
 import upcoming from './_upcoming';
 import year from './_year';
@@ -16,7 +15,6 @@ import year from './_year';
 export default {
     month,
     _namedDates,
-    period,
     previous,
     upcoming,
     year
--- src/methods/numbers/_between.js
+++ src/methods/numbers/_between.js
@@ -3,6 +3,7 @@
  ---------------------------------------- */
 
 import evaluate from '@util/_evaluate';
+import isType from '@util/_isType';
 
 /** ----------------------------------------
     Between
@@ -21,9 +22,10 @@ import evaluate from '@util/_evaluate';
 
 function between(key, values, date = false) {
     this._hasTrail();
+    const isDate = isType(values[0]) === 'date';
 
     const filter = this._data.filter(item => {
-        return evaluate(item[key], '>=', values[0], date) && evaluate(item[key], '<=', values[1], date);
+        return evaluate(item[key], '>=', values[0], isDate || date) && evaluate(item[key], '<=', values[1], isDate || date);
     });
 
     return this._chain(filter);
