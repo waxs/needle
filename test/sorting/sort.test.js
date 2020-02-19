b00f47f81fb5042b03e753fbd8bf506eede354f8
waxs <sander@vruchtvlees.com>
2020-02-19 18:47:57 +0100

    ✅ add additional sort() testing

--- test/sorting/sort.test.js
+++ test/sorting/sort.test.js
@@ -15,8 +15,8 @@ const needle = new Needle(data);
      Test sort()
  ---------------------------------------- */
 
-describe('Using sort() method for sorting items', () => {
-    test('It should display sorted array', () => {
+describe('Using sort() method for sorting numbers', () => {
+    test('It should display items sorted on age', () => {
         const input = data;
 
         const output = [
@@ -36,6 +36,62 @@ describe('Using sort() method for sorting items', () => {
 
         const query = needle
             .sort('age')
+            .select('name', 'age')
+            .take();
+
+        expect(query).toEqual(output);
+    });
+});
+
+describe('Using sort() method for sorting strings', () => {
+    test('It should display items sorted on name', () => {
+        const input = data;
+
+        const output = [
+            {
+                name: 'John',
+                age: 45
+            },
+            {
+                name: 'Nicole',
+                age: 32
+            },
+            {
+                name: 'Sander',
+                age: 30
+            }
+        ];
+
+        const query = needle
+            .sort('name')
+            .select('name', 'age')
+            .take();
+
+        expect(query).toEqual(output);
+    });
+});
+
+describe('Using sort() method for sorting dates', () => {
+    test('It should display items sorted on created', () => {
+        const input = data;
+
+        const output = [
+            {
+                name: 'Nicole',
+                age: 32
+            },
+            {
+                name: 'John',
+                age: 45
+            },
+            {
+                name: 'Sander',
+                age: 30
+            }
+        ];
+
+        const query = needle
+            .sort('created')
             .select('name', 'age')
             .take();
 
