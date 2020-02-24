9921fe66933f0c81b30b9cafc1025e757cf9ddce
waxs <sander@vruchtvlees.com>
2020-02-24 18:35:14 +0100

    🤡 add vue example

---
+++ mockup/extended.js
@@ -0,0 +1,195 @@
+/** ----------------------------------------
+     Mockup Data
+ ---------------------------------------- */
+
+const data = [
+    {
+        active: true,
+        created: '12/2/2020',
+        name: 'Sander',
+        age: 30,
+        city: 'Amsterdam',
+        hobbies: [
+            'Music',
+            'Movies'
+        ]
+    },
+    {
+        active: false,
+        created: '10/1/2017',
+        name: 'Nicole',
+        age: 32,
+        city: 'Paris',
+        hobbies: [
+            'Dancing'
+        ]
+    },
+    {
+        active: true,
+        created: '7/10/2019',
+        name: 'John',
+        age: 45,
+        city: 'New York',
+        hobbies: [
+            'Sports',
+            'Books'
+        ]
+    },
+    {
+        active: false,
+        created: '3/20/2019',
+        name: 'Sara',
+        age: 26,
+        city: 'Rome',
+        hobbies: [
+            'Sports',
+            'Music'
+        ]
+    },
+    {
+        active: false,
+        created: '3/20/2020',
+        name: 'Peter',
+        age: 43,
+        city: 'London',
+        hobbies: [
+            'Music',
+            'Dancing',
+            'Games'
+        ]
+    },
+    {
+        active: true,
+        created: '10/12/2018',
+        name: 'Lisa',
+        age: 55,
+        city: 'Glasgow',
+        hobbies: [
+            'Books'
+        ]
+    },
+    {
+        active: true,
+        created: '7/23/2019',
+        name: 'Stephen',
+        age: 22,
+        city: 'Berlin',
+        hobbies: [
+            'Books',
+            'Sports',
+            'Music'
+        ]
+    },
+    {
+        active: true,
+        created: '5/1/2018',
+        name: 'Lauren',
+        age: 18,
+        city: 'Moscow',
+        hobbies: [
+            'TV Series',
+            'Movies'
+        ]
+    },
+    {
+        active: true,
+        created: '7/23/2019',
+        name: 'Phillip',
+        age: 27,
+        city: 'London',
+        hobbies: [
+            'TV Series',
+            'Books',
+            'Walking'
+        ]
+    },
+    {
+        active: true,
+        created: '7/23/2019',
+        name: 'Trevor',
+        age: 38,
+        city: 'San Francisco',
+        hobbies: [
+            'Movies',
+            'Music',
+            'Games'
+        ]
+    },
+    {
+        active: true,
+        created: '7/2/2020',
+        name: 'Simon',
+        age: 28,
+        city: 'Rome',
+        hobbies: [
+            'Movies',
+            'Games'
+        ]
+    },
+    {
+        active: true,
+        created: '1/1/2019',
+        name: 'Amber',
+        age: 23,
+        city: 'Berlin',
+        hobbies: [
+            'Games'
+        ]
+    },
+    {
+        active: true,
+        created: '11/5/2019',
+        name: 'Ali',
+        age: 44,
+        city: 'Ankara',
+        hobbies: [
+            'Games',
+            'Music'
+        ]
+    },
+    {
+        active: true,
+        created: '2/5/2020',
+        name: 'John',
+        age: 37,
+        city: 'Liverpool',
+        hobbies: [
+            'Walking',
+            'Music'
+        ]
+    },
+    {
+        active: true,
+        created: '12/13/2019',
+        name: 'Clara',
+        age: 41,
+        city: 'Brussel',
+        hobbies: [
+            'Books',
+            'Walking',
+            'Music'
+        ]
+    },
+    {
+        active: false,
+        created: '12/7/2019',
+        name: 'Xavier',
+        age: 53,
+        city: 'Madrid',
+        hobbies: [
+            'Movies',
+            'Books'
+        ]
+    },
+    {
+        active: false,
+        created: '3/21/2018',
+        name: 'Bridgette',
+        age: 41,
+        city: 'Marseille',
+        hobbies: [
+            'Walking',
+            'Books'
+        ]
+    }
+];
